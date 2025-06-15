500929fff527a106c2e72d0dca3913f7de86ebf7
very-blank <aapeli.saarelainen.76@gmail.com>
2025-06-16 00:10:03 +0300

    changes

--- build.zig
+++ build.zig
@@ -28,6 +28,29 @@ pub fn build(b: *std.Build) void {
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
+    // Tests:
+    const testFiles = .{
+        "escTest",
+    };
+
+    const test_step = b.step("test", "Run unit tests");
+
+    inline for (testFiles) |fileName| {
+        const testModule = b.createModule(.{
+            .root_source_file = b.path("src/" ++ fileName ++ ".zig"),
+            .target = target,
+            .optimize = optimize,
+        });
+
+        const unitTest = b.addTest(.{
+            .root_module = testModule,
+        });
+
+        const runUnitTest = b.addRunArtifact(unitTest);
+
+        test_step.dependOn(&runUnitTest.step);
+    }
+
     // const exe_unit_tests = b.addTest(.{
     //     .root_module = exe_mod,
     // });
