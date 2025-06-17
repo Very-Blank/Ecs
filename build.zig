97ca012493f9dd3c4b3e0ea3d9b42d8bbaed1e4d
very-blank <aapeli.saarelainen.76@gmail.com>
2025-06-17 14:41:07 +0300

    fix: exe -> lib

--- build.zig
+++ build.zig
@@ -4,29 +4,19 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const exe_mod = b.createModule(.{
+    const qoi = b.createModule(.{
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
 
-    const exe = b.addExecutable(.{
-        .name = "ECS",
-        .root_module = exe_mod,
+    const lib = b.addLibrary(.{
+        .linkage = .static,
+        .name = "Ecs",
+        .root_module = qoi,
     });
 
-    b.installArtifact(exe);
-
-    const run_cmd = b.addRunArtifact(exe);
-
-    run_cmd.step.dependOn(b.getInstallStep());
-
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
-
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
+    b.installArtifact(lib);
 
     // Tests:
     const testFiles = .{
@@ -50,13 +40,4 @@ pub fn build(b: *std.Build) void {
 
         test_step.dependOn(&runUnitTest.step);
     }
-
-    // const exe_unit_tests = b.addTest(.{
-    //     .root_module = exe_mod,
-    // });
-    //
-    // const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
-    //
-    // const test_step = b.step("test", "Run unit tests");
-    // test_step.dependOn(&run_exe_unit_tests.step);
 }
