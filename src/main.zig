8eb1e338e71842f93f61e9415715fd305712b94a
very-blank <aapeli.saarelainen.76@gmail.com>
2025-06-20 21:23:01 +0300

    fix: TupleIterator not in main

--- src/main.zig
+++ src/main.zig
@@ -1,3 +1,4 @@
 pub const Ecs = @import("ecs.zig").Ecs;
 pub const SlimPointer = @import("entity.zig").SlimPointer;
 pub const Iterator = @import("iterator.zig").Iterator;
+pub const TupleIterator = @import("iterator.zig").TupleIterator;
