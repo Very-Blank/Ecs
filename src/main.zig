4ce1eb26eb4ded4095821bf8f0a420bec5676d75
very-blank <aapeli.saarelainen.76@gmail.com>
2025-06-26 20:58:53 +0300

    fix: EntityType not visible

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,6 @@
 pub const Ecs = @import("ecs.zig").Ecs;
 pub const SlimPointer = @import("entity.zig").SlimPointer;
+pub const EntityType = @import("entity.zig").EntityType;
 pub const SingletonType = @import("singletonManager.zig").SingletonType;
 pub const Iterator = @import("iterator.zig").Iterator;
 pub const TupleIterator = @import("iterator.zig").TupleIterator;
