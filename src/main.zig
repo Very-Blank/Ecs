8d8415f648da89bb8b2337719f0eb107fe92b42a
very-blank <aapeli.saarelainen.76@gmail.com>
2025-09-06 20:12:03 +0300

    fix: Template not in main.

--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,5 @@
 pub const Ecs = @import("ecs.zig").Ecs;
+pub const Template = @import("ecs.zig").Template;
 pub const EntityPointer = @import("ecs.zig").EntityPointer;
 pub const EntityType = @import("ecs.zig").EntityType;
 pub const SingletonType = @import("ecs.zig").SingletonType;
